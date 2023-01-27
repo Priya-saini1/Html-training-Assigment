1a0c08294df5d51eab7f78c45feb0f2e3f7e820a
priya rajbahadur saini <erpriyasaini2000@gmail.com>
2023-01-27 18:28:24 +0530

    first-commit

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+function dasbboard(){
+    document.getElementById("Dashborad-active").src="images/dashboard-active.svg";
+}
+
+function analysis(){
+    document.getElementById("Analysis-active").src="images/graph-active.svg";
+}
+
