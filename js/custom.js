e2ab1e80fdc261ecf06b59a98d120c94d16cbbe4
priya rajbahadur saini <erpriyasaini2000@gmail.com>
2023-01-30 18:23:23 +0530

    commit

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,25 @@ function analysis(){
     document.getElementById("Analysis-active").src="images/graph-active.svg";
 }
 
+function focusform(){
+    document.getElementById("focusbtns").style.borderColor="#C8242F";
+}
+
+function fileuploaded(){
+    var msg = document.getElementById("upload");
+    var file = document.getElementById("custombuttonid");
+    if(file.files.length == 0)
+    {
+        msg.innerHTML="No item Selected".fontcolor("#ADADAD");
+    }
+    else{
+        msg.innerHTML="Item Selected".fontcolor("green");
+    }
+}
+
+function designationfun(){
+    document.getElementById("Designationbtns").style.borderColor="#C8242F";
+
+}
+
+
